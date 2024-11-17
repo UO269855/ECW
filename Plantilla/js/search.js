d7ea5f1b1f2c85e152d0e48a1fccd8f5a14e12f3
aleex <uo269855@uniovi.es>
2024-11-17 14:54:10 +0100

    sadsa

--- Plantilla/js/search.js
+++ Plantilla/js/search.js
@@ -59,8 +59,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Function to display the results for a file
     function displayResults(file, sentence) {
       const fileName = file.split("/").pop(); // Get the file name
-      const fileLink =
-        "<a href=" + file + 'target="_blank">' + fileName + "</a>";
+      const fileLink = "<a href=" + file + ">" + fileName + "</a>";
       let resultHTML = '<article class="result">' + fileLink + "</article>";
       resultHTML += '<article class="result"><p>' + sentence + "</p></article>";
 
